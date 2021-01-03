32df9b67c4064dad6cd792eb9295358dc0eec942
xopherw <xopher95@gmail.com>
2021-01-03 15:44:30 +0800

    add [im in] command and gif_link.txt for random gifs

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import discord
-import os
+import discord, os, random
 from discord.ext import commands
-import re
 
 bot = commands.Bot(command_prefix='$')
 
- # Bot prints embedded message from any user's input, will return to original input user's name if arg3 is not mentioned
+# Bot prints embedded message from any user's input, will return to original input user's name if arg3 is not mentioned
 @bot.command(name='idea')
 async def isb(ctx, arg1, arg2, arg3 = ''):
-  channel = bot.get_channel('Channel ID integer')
-  name = ctx.author if(arg3 is '') else arg3
+  channel = bot.get_channel(785290698710056990)
+  name = ctx.author if(arg3 == '') else arg3
   myEmbed = discord.Embed(title=arg1, description=arg2, color=0x00ff00)
   myEmbed.set_author(name=name)
   await channel.send(embed=myEmbed)
   await ctx.message.author.send(f'Your **{arg1}** idea has been added!')
 
-@bot.command(name='version')  # Bot prints version numbers and creators
+# Bot prints version numbers and creators
+@bot.command(name='version')
 async def idea(ctx):
   myEmbed = discord.Embed(title="Current Version", description='The bot is in Version 1.0' , color=0xff0000)
   myEmbed.add_field(name="Version Code:", value="v1.0.0", inline=False)
@@ -23,21 +22,22 @@ async def idea(ctx):
   myEmbed.set_footer(text="Created by OsamaBinNaughty & Chrisadilla")
   await ctx.message.channel.send(embed= myEmbed)
 
+# Bot returns "mentioned-user you're in!" with a gif
+@bot.command(name="im") 
+async def im_in(ctx, argIn):
+  channel = bot.get_channel(785290698710056990)
+  if(argIn == 'in'):
+    await channel.send(f"<@{ctx.author.id}> you're in!")
+    await channel.send(f"{random.choice(open(os.getcwd()+'/gif_links.txt','r').readlines())}")
+
+# Initial state of the bot, will say "Beep beep boob, I am alive!"
 @bot.event
-async def on_ready(): # Initial state of the bot, will say "Beep beep boob, I am alive!"
+async def on_ready(): 
   print('We have logged in as {0.user}'
   .format(bot))
   botTest_channel=bot.get_channel(785290698710056990)
   await botTest_channel.send('Beep beep boob, I am alive!')
   await bot.change_presence(status=discord.Status.online, activity='Brainstorming')
 
-# @bot.event
-# async def on_message(message):
-#   botTest_channel=bot.get_channel(785290698710056990)
-#   if message.author == bot.user:
-#     return
+bot.run('NzkzNjY4NzI2Nzk1OTI3NTcy.X-vnmg.pUZgrRlsAGhIR4_kNtqxk-l549Y')
 
-#   await bot.process_commands(message)
-  
-
-bot.run('TOKEN')
